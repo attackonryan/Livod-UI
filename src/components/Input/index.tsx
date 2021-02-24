a125a19bf54a12787d4875669209d26f43c9994f
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-24 15:17:57 +0800

    fix: 修复了Input组件不支持style的问题

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { CSSProperties, useMemo } from "react";
 import TextArea from "./TextArea";
 import "./style/index.less";
 export interface OriginInputProps {
@@ -9,6 +9,7 @@ export interface OriginInputProps {
   /** 是否有错(影响样式) */
   hasError?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  style?: CSSProperties;
 }
 
 const OriginInput: React.FC<OriginInputProps> & {
